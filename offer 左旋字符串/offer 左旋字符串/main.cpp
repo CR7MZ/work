8cdace309e32a4b63d7800ea72d8b90151e3341f
CR7MZ <963835264@qq.com>
2020-02-24 17:18:44 +0800

    翻转单词顺序列

---
+++ offer 左旋字符串/offer 左旋字符串/main.cpp
@@ -0,0 +1,37 @@
+//题目：https://www.nowcoder.com/questionTerminal/12d959b108cb42b1ab72cef4d36af5ec?f=discussion
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+
+//思路：
+//用队列，将字符串放入队列中，出队n次，将出队的数据按顺序放回队列尾部。将队列中的字符合而成字符串。
+string LeftRotateString(string str, int n) {
+	queue<char> q;
+	for (auto e : str)
+	{
+		q.push(e);
+	}
+	while (n)
+	{
+		int tmp = q.front();
+		q.pop();
+		q.push(tmp);
+		n--;
+	}
+	for (int i = 0; i<str.size(); i++)
+	{
+		str[i] = q.front();
+		q.pop();
+	}
+	return str;
+}
+
+//大神思路：
+//将字符串旋转两遍，可以找个例子模拟一下。
+string LeftRotateString(string str, int n) {
+	reverse(str.begin(), str.end());
+	reverse(str.begin(), str.begin() + str.size() - n);
+	reverse(str.begin() + str.size() - n, str.end());
+	return str;
+}
