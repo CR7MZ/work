1cd3f1c2e12f8e99f37589763a6e9a50fa5a432e
CR7MZ <963835264@qq.com>
2020-03-01 14:21:21 +0800

    字符流中第一个不重复的字符

---
+++ offer 字符流中第一个不重复的字符/offer 字符流中第一个不重复的字符/main.cpp
@@ -0,0 +1,28 @@
+//题目：
+#include <iostream>
+#include <queue>
+
+//思路：
+//定义一个数组来对字符进行计数，再定义一个队列，将第一次出现的字符放入，最后通过获取队列头元素来通过数组判断是不是只出现一次的字符
+//若是，则此字符就是第一个不重复出现的字符。
+int v[256];
+std::queue<char> tmp;
+
+void Insert(char ch)
+{
+	v[ch]++;
+	if (v[ch] == 1)
+		tmp.push(ch);
+}
+char FirstAppearingOnce()
+{
+	while (!tmp.empty() && v[tmp.front()]>1)
+		tmp.pop();
+	if (tmp.empty()) return '#';
+	return tmp.front();
+}
+void Func()
+{
+	memset(v, 0, sizeof(v));//给数组赋值为0
+}
+
