b73c630bbb68f544c365b7ad64b1de6634858ca1
CR7MZ <963835264@qq.com>
2020-03-30 18:09:09 +0800

    文件压缩

--- 压缩项目/压缩项目/main.cpp
+++ 压缩项目/压缩项目/main.cpp
@@ -1,13 +1,19 @@
 #include "fileCompressHuff.h"
-#include"LZ77.h"
+#include "LZ77.h"
+#include <windows.h>
+#include <time.h>
 int main()
 {
 	LZ77 LZ;
 	filecompressHuff f;
+	time_t begin, end;
+	begin = time(NULL);
 	LZ.Compressfile("1.txt");
 	f.compressfile("2.lzp");
 	f.UNcompressfile("3.lzp");
 	LZ.Uncompressfile("4.lzp");
+	end = time(NULL);
+	cout << end - begin << endl;
 	system("pause");
 	return 0;
 }
