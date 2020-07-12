201430359cf68ffd9bd4dc9aa0b4b5f727366973
CR7MZ <963835264@qq.com>
2020-07-12 14:34:44 +0800

    offer 数组中重复的数字

--- offer 数组中重复的数字/offer 数组中重复的数字/main.cpp
+++ offer 数组中重复的数字/offer 数组中重复的数字/main.cpp
@@ -1,19 +1,16 @@
-//题目：https://www.nowcoder.com/questionTerminal/623a5ac0ea5b4e5f95552655361ae0a8?f=discussion
+//题目：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/comments/
 #include<iostream>
 #include<vector>
-//定义一个存放bool类型的数组v，初始化为false
-//把numbers中的数当做v的下标，numbers中的数出现了，就将相应位置置为true。
-//若在V中找到值为true的位置，将位置返回，此为第一个重复出现的数字。
+
+//采用数组哈希的方式，因为题目规定数字的范围是0~n-1
+//因此定义一个size为nums.size（）的数组，模拟哈希的方式
+//时间复杂度O(N)，空间复杂度O(N)
 bool duplicate(int numbers[], int length, int* duplication) {
-	std::vector<bool> v(length, false);
-	for (int i = 0; i<length; i++)
-	{
-		if (v[numbers[i]] == true)
-		{
-			*duplication = numbers[i];
-			return true;
-		}
-		v[numbers[i]] = true;
-	}
-	return false;
+	vector<int> tmp(nums.size());
+        for (int i = 0;i < nums.size();i++){
+            tmp[nums[i]]++;
+            if (tmp[nums[i]] > 1)
+                return nums[i];
+        }
+	    return 0;
 }
