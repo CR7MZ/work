07da0ede253a5a707431ffe603aa5ef20a8230e7
CR7MZ <963835264@qq.com>
2020-10-10 11:15:22 +0800

    常考：实现开平方

--- 常考：实现开平方/面试常考：实现开平方/main.cpp
+++ 常考：实现开平方/面试常考：实现开平方/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//https://blog.csdn.net/xusiwei1236/article/details/25657611
+
 double My_Sqrt(double x)
 {
-	
+	double a = 0.0, b = x + 0.25, mid;
+	while (1){
+		mid = (a + b) / 2;
+		if (mid - a < DBL_EPSILON || b - mid < DBL_EPSILON) break;
+		if ((mid * mid - x) * (a * a - x) < 0){
+			b = mid;
+		}else {
+			a = mid;
+		}
+	}
+	return mid;
 }
 
 int main()
 {
-	int x;
+	double x = 25;
+	cout << My_Sqrt(x) << endl;
 	return 0;
 }
